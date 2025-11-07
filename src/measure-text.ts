d4601b84b6b0974747d522f462be7166ae2b918b
jacob314 <jacob314@gmail.com>
2025-11-06 20:38:25 -0800

    Disable styles.

--- src/measure-text.ts
+++ src/measure-text.ts
@@ -1,10 +1,7 @@
 import stringWidth from 'string-width';
 import {
-	tokenize,
-	styledCharsFromTokens,
 	type StyledChar,
 } from '@alcalzone/ansi-tokenize';
-import {DataLimitedLruMap} from './data-limited-lru-map.js';
 
 export type StringWidth = (text: string) => number;
 
@@ -22,138 +19,19 @@ export function clearStringWidthCache() {
 }
 
 export function toStyledCharacters(text: string): StyledChar[] {
-	const tokens = tokenize(text);
-	const characters = styledCharsFromTokens(tokens);
-	const combinedCharacters: StyledChar[] = [];
+	const characters: StyledChar[] = [];
 
-	for (let i = 0; i < characters.length; i++) {
-		const character = characters[i];
-		if (!character) {
-			continue;
-		}
-
-		if (character.value === '\t') {
-			const spaceCharacter: StyledChar = {...character, value: ' '};
-
-			combinedCharacters.push(
-				spaceCharacter,
-				spaceCharacter,
-				spaceCharacter,
-				spaceCharacter,
-			);
-			continue;
-		}
-
-		if (character.value === '\b') {
-			continue;
-		}
-
-		let {value} = character;
-		let isCombined = false;
-		const firstCodePoint = value.codePointAt(0);
-
-		// 1. Regional Indicators (Flags)
-		// These combine in pairs.
-		// See: https://en.wikipedia.org/wiki/Regional_indicator_symbol
-		if (
-			firstCodePoint &&
-			firstCodePoint >= 0x1_f1_e6 &&
-			firstCodePoint <= 0x1_f1_ff &&
-			i + 1 < characters.length
-		) {
-			const nextCharacter = characters[i + 1];
-
-			if (nextCharacter) {
-				const nextFirstCodePoint = nextCharacter.value.codePointAt(0);
-
-				if (
-					nextFirstCodePoint &&
-					nextFirstCodePoint >= 0x1_f1_e6 &&
-					nextFirstCodePoint <= 0x1_f1_ff
-				) {
-					value += nextCharacter.value;
-					i++;
-
-					combinedCharacters.push({...character, value});
-					continue;
-				}
-			}
-		}
-
-		// 2. Other combining characters
-		// See: https://en.wikipedia.org/wiki/Combining_character
-		while (i + 1 < characters.length) {
-			const nextCharacter = characters[i + 1];
-
-			if (!nextCharacter) {
-				break;
-			}
-
-			const codePoints = [...nextCharacter.value].map(char =>
-				char.codePointAt(0),
-			);
-
-			const nextFirstCodePoint = codePoints[0];
-
-			if (!nextFirstCodePoint) {
-				break;
-			}
-
-			// Variation selectors
-			const isVariationSelector =
-				nextFirstCodePoint >= 0xfe_00 && nextFirstCodePoint <= 0xfe_0f;
-
-			// Skin tone modifiers
-			const isSkinToneModifier =
-				nextFirstCodePoint >= 0x1_f3_fb && nextFirstCodePoint <= 0x1_f3_ff;
-
-			const isZeroWidthJoiner = nextFirstCodePoint === 0x20_0d;
-			const isKeycap = nextFirstCodePoint === 0x20_e3;
-
-			// Tags block (U+E0000 - U+E007F)
-			const isTagsBlock =
-				nextFirstCodePoint >= 0xe_00_00 && nextFirstCodePoint <= 0xe_00_7f;
-
-			// Combining Diacritical Marks
-			const isCombiningMark =
-				nextFirstCodePoint >= 0x03_00 && nextFirstCodePoint <= 0x03_6f;
-
-			const isCombining =
-				isVariationSelector ||
-				isSkinToneModifier ||
-				isZeroWidthJoiner ||
-				isKeycap ||
-				isTagsBlock ||
-				isCombiningMark;
-
-			if (!isCombining) {
-				break;
-			}
-
-			// Merge with previous character
-			value += nextCharacter.value;
-			i++; // Consume next character.
-			isCombined = true;
-
-			// If it was a ZWJ, also consume the character after it.
-			if (isZeroWidthJoiner && i + 1 < characters.length) {
-				const characterAfterZwj = characters[i + 1];
-
-				if (characterAfterZwj) {
-					value += characterAfterZwj.value;
-					i++; // Consume character after ZWJ.
-				}
-			}
-		}
-
-		if (isCombined) {
-			combinedCharacters.push({...character, value});
-		} else {
-			combinedCharacters.push(character);
-		}
+	const styles: StyledChar['styles'] = [];
+	for (const char of text) {
+		characters.push({
+			type: 'char',
+			value: char,
+			fullWidth: false,
+			styles: styles,
+		});
 	}
 
-	return combinedCharacters;
+	return characters;
 }
 
 export function styledCharsWidth(styledChars: StyledChar[]): number {
