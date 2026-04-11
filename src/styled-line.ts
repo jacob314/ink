1e3a13fc3ca8b8f44f066de1ec2fc65f16b7e916
jacob314 <jacob314@gmail.com>
2026-04-11 11:27:44 -0700

    Optimize measure-text and styled-line.ts to reduce time from 1.97s to 1.31s

--- src/styled-line.ts
+++ src/styled-line.ts
@@ -331,6 +331,75 @@ export class StyledLine {
 			this.ensureSpans();
 		}
 
+		// Fast path: Find the span containing this index
+		let currentOffset = 0;
+		let spanIndex = -1;
+		let span: StyleSpan | undefined;
+
+		for (let i = 0; i < this.spans!.length; i++) {
+			const s = this.spans![i]!;
+			if (currentOffset <= index && currentOffset + s.length > index) {
+				spanIndex = i;
+				span = s;
+				break;
+			}
+			currentOffset += s.length;
+		}
+
+		if (span) {
+			const isMatch =
+				span.formatFlags === cleanFormatFlags &&
+				span.fgColor === fgColor &&
+				span.bgColor === bgColor &&
+				span.link === link;
+
+			if (isMatch) {
+				return; // Style already matches, no need to touch spans
+			}
+
+			// If it's the very first character of the span, check if we can merge with the previous span
+			if (index === currentOffset && spanIndex > 0) {
+				const prevSpan = this.spans![spanIndex - 1]!;
+				const prevMatch =
+					prevSpan.formatFlags === cleanFormatFlags &&
+					prevSpan.fgColor === fgColor &&
+					prevSpan.bgColor === bgColor &&
+					prevSpan.link === link;
+
+				if (prevMatch) {
+					prevSpan.length += 1;
+					if (span.length === 1) {
+						this.spans!.splice(spanIndex, 1);
+					} else {
+						span.length -= 1;
+					}
+					this._spansDirty = true;
+					return;
+				}
+			}
+
+			// If it's the very last character of the span, check if we can merge with the next span
+			if (index === currentOffset + span.length - 1 && spanIndex < this.spans!.length - 1) {
+				const nextSpan = this.spans![spanIndex + 1]!;
+				const nextMatch =
+					nextSpan.formatFlags === cleanFormatFlags &&
+					nextSpan.fgColor === fgColor &&
+					nextSpan.bgColor === bgColor &&
+					nextSpan.link === link;
+
+				if (nextMatch) {
+					nextSpan.length += 1;
+					if (span.length === 1) {
+						this.spans!.splice(spanIndex, 1);
+					} else {
+						span.length -= 1;
+					}
+					this._spansDirty = true;
+					return;
+				}
+			}
+		}
+
 		this.splitSpansAt(index);
 		this.splitSpansAt(index + 1);
 
